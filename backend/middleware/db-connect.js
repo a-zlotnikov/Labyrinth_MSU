c2e598fae024f1fdaa65e41e8305f6cc9392a86e
Ilya Mikhaylov <doxxmsk@gmail.com>
2020-01-23 15:28:17 +0300

    (back) sign up - needs testing

--- backend/middleware/db-connect.js
+++ backend/middleware/db-connect.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // Данное поле будет заменено на актуальный сервер при его создании
 mongoose.connect('mongodb+srv://admin:LnCC67f8@labirynth-420rj.mongodb.net',
-  { useNewUrlParser: true, useUnifiedTopology: true });
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = mongoose.connection;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,57 @@
 const express = require('express');
 const router = express.Router();
+// const db = require('../middleware/db-connect');
+const User = require('../models/user');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const morgan = require('morgan');
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+// const bcryptjs = require('bcryptjs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', async function(req, res, next) { // Регистрация нового пользователя
+  console.log('>>> SIGNUP');
+  console.log(req.body);
+  try {
+    /*const {
+      username,
+      password,
+      category,
+      surname,
+      name,
+      gender,
+      dob,
+      hand,
+      group,
+      year,
+    } = req.body;*/
+    const user = new User({...req.body});
+    await user.save();
+    req.session.user = user;
+    await res.json({logged_in: true});
+  } catch (e) {
+    await res.json({logged_in: false});
+  }
+});
+
+router.post('/users/signin', async function(req, res, next) {
+  try {
+    const {username, password} = req.body;
+    const user = User.find({username});
+    if (user.password === password) {
+      req.session.user = user;
+      await res.json({logged_in: true});
+    } else {
+      await res.json({logged_in: false});
+    }
+  } catch (e) {
+    await res.json({logged_in: false});
+  }
 });
 
 module.exports = router;
