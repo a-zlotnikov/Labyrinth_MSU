3ddd5c3bf87e96043af98bf15fcf8350271e2af1
Konstantin <xreikox@mail.ru>
2020-01-24 14:34:22 +0300

    add Switch component in App

--- frontend/src/store/creators/creators.js
+++ frontend/src/store/creators/creators.js
@@ -1,36 +1,36 @@
-import {FULL_FIELD, ADD_VALUE, CHANGE_VALUE} from "../actions/actions";
+import {FULL_FIELD, ADD_VALUE, CHANGE_VALUE} from '../actions/actions';
 
 export const FULLFIELD = (field) => {
-    return {
-        type: FULL_FIELD,
-        field
-    }
+  return {
+    type: FULL_FIELD,
+    field,
+  };
 };
 
 export const ADDVALUE = (value, change) => {
-    return {
-        type: ADD_VALUE,
-        value,
-        change
-    }
+  return {
+    type: ADD_VALUE,
+    value,
+    change,
+  };
 };
 
 export const CHANGEVALUE = (value, changedValue) => {
-    return {
-        type: CHANGE_VALUE,
-        value,
-        changedValue
-    }
+  return {
+    type: CHANGE_VALUE,
+    value,
+    changedValue,
+  };
 };
 
 export const fetchField = () => {
-    return async (dispatch) => {
-        console.log(123);
-        const response = await fetch('/getField');
-        const result = await response.json();
-        console.log(result);
-        dispatch(FULLFIELD(result))
-    }
+  return async (dispatch) => {
+    console.log(123);
+    const response = await fetch('/getField');
+    const result = await response.json();
+    console.log(result);
+    dispatch(FULLFIELD(result));
+  };
 };
 
 
