7826ccc6a43535fa95c624237e1f524e00632307
AidynSC <ay_dyn@mail.ru>
2020-01-24 14:28:45 +0300

    new branch new life

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,45 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import Layout from './Layout/Layout';
-import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/SignIn';
 import Mainpage from './containers/mainpage/mainpage';
 import Navbar from './containers/Navbar/Navbar';
 import Field from './components/Field/Field';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      user: '',
+      loading: true
+    };
+  }
+  
+  componentDidMount = async () => {
+    this.setState({loading: true});
+    const response = await fetch('/users');
+    const result = await response.json();
+    this.setState({user: result, loading: false});
+    console.log(this.state.user)
+  };
+  
   render() {
-    return (
-      <Layout>
+    return (this.state.loading === true) ? (<div>Loading...</div>) :
+      (this.state.user.user === undefined) ? (<div><Route path={'/'} component={SignIn}/></div>) :
+        (<Layout>
         <div>
-          <Navbar/>
-          <Mainpage/>
-          <Field />
+          <Route render={(props) =>{
+            return (
+              <div>
+                <Navbar {...props} username={this.state.user.user.username} />
+              </div>
+            );
+          }} />
+          <Route path={'/'} component={Mainpage}/>
         </div>
-        <SignUp/>
       </Layout>
-    );
+    )
   }
 }
 
