bc80cecfef09025a11c749338829716be3b978c8
Ilya Mikhaylov <doxxmsk@gmail.com>
2020-01-27 16:30:00 +0300

    (front) cookies - welcome back

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -84,4 +84,18 @@ router.delete('/delete', async function(req, res) {
   }
 });
 
+router.get ('/logout', async (req, res, next) => {
+  if (req.session.user) {
+    try {
+      await req.session.destroy ()
+      res.clearCookie ('user_sid')
+      res.redirect ('/')
+    } catch (error) {
+      next (error)
+    }
+  } else {
+    res.redirect ('/login')
+  }
+})
+
 module.exports = router;
