5612c37391771dd535aedd9157c26a329fb155f6
Ilya Mikhaylov <doxxmsk@gmail.com>
2020-01-27 11:13:54 +0300

    (front) cookies

--- frontend/src/containers/Navbar/Navbar.js
+++ frontend/src/containers/Navbar/Navbar.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import classes from './Navbar.module.css';
+const Cookies = require('js-cookie');
 
 class Navbar extends Component {
   render() {
     return (
       <div className={classes.Navbar}>
         <Link to={'/'}>Главное меню</Link>
-        <div>{this.props.options.username} /
-          {this.props.options.category} /
-          {this.props.options.group ?
-          this.props.options.group : null}</div>
+        <div>{Cookies.get('surname')} {Cookies.get('name')} / {/*{this.props.options.category} /*/} {/*{this.props.options.group ?*/} {/*this.props.options.group : null}*/}
+        </div>
       </div>
     );
   }
