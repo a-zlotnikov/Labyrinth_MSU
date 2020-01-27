9e89a5008ad7cb0f2c55a1dfb4d0534edcbb2cd7
Konstantin <xreikox@mail.ru>
2020-01-27 11:45:14 +0300

    fix

--- frontend/src/containers/mainpage/mainpage.js
+++ frontend/src/containers/mainpage/mainpage.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import Logo from './Logo/Logo';
 import AdminPage from './adminPage/adminPage';
 import OtherPage from './otherPage/otherPage';
+import Cookies from 'js-cookie';
 
 class Mainpage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      category: this.props.options.category,
+      category: Cookies.get('category'),
       isAdmin: ['Преподаватель'],
       isNotAdmin: ['Студент', 'Диплонник'],
     };
