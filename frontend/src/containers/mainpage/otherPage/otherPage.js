0f94fd6793e9cd0b821007c11daabf747d475e2b
Konstantin <xreikox@mail.ru>
2020-01-29 11:52:37 +0300

    change button location in menu

--- frontend/src/containers/mainpage/otherPage/otherPage.js
+++ frontend/src/containers/mainpage/otherPage/otherPage.js
@@ -6,7 +6,6 @@ class OtherPage extends Component {
   render() {
     return (
       <div className={classes.OtherPage}>
-        <Link to={'/constructor'}>Конструктор</Link>
         <Link to={'/environment'}>Архив сред</Link>
         <Link to={'/results'}>Архив экспериментов</Link>
       </div>
