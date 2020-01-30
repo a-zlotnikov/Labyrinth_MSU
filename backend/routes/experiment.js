4ecbccd25c916bf8ca03f4e9787d0793b6637484
Konstantin <xreikox@mail.ru>
2020-01-30 10:01:11 +0300

    fix refresh bug in results under delete element

--- backend/routes/experiment.js
+++ backend/routes/experiment.js
@@ -9,13 +9,11 @@ router.get('/', async (req, res) => {
 
 router.post('/search', async function(req, res) {
   try {
-    console.log(req.body);
     let result = '';
     req.body.query === result ?
       result = await Experiment.find() :
       result = await Experiment.find({[req.body.type]: req.body.query});
     await res.json({response: result});
-    console.log(result);
   } catch (e) {
     await res.json({response: false});
   }
@@ -28,7 +26,7 @@ router.post('/', async function(req, res) {
 
 router.delete('/', async function(req, res) {
   const deleteElem = await Experiment.findOneAndDelete({_id: req.body.id});
-  const newResults = await Experiment.find();
+  const newResults = await Experiment.find().populate('user');
   res.json(newResults);
 });
 
